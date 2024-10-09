e0dbed26cec8dcf69aeecba03f8e8537d99f895f
Gustavo Veiga São Leandro <55115090+guta307@users.noreply.github.com>
2024-10-09 16:53:41 -0300

    feat: Add routes for transaction and item operations
    - Added route for creating a transaction at `/transaction/create`.
    - Added route for updating a transaction at `/transaction/update`.
    - Added route for adding items to a transaction at `/transaction/add`.
    - Added route for removing items from a transaction at `/transaction/remove`.
    - Added route for listing all transactions at `/transaction/list`.
    - Added route for listing all items at `/item/list`.
    - Added route to find a transaction by ID at `/transaction/findById`.
    - Added route to find an item by ID at `/item/findById`.

--- src/app/routes/transactionRoute.ts
+++ src/app/routes/transactionRoute.ts
@@ -6,20 +6,38 @@ const router = Router();
 
 router.post(
   "/transaction/create",
-  TransactionController.addItem.bind(TransactionController)
+  TransactionController.createItem.bind(TransactionController)
 );
 
 router.post(
   "/transaction/update",
   TransactionController.updateItem.bind(TransactionController)
 );
-~~router.get(
+router.post(
+  "/transaction/add",
+  TransactionController.addItem.bind(TransactionController)
+);
+router.post(
+  "/transaction/remove",
+  TransactionController.subtractItem.bind(TransactionController)
+);
+router.get(
   "/transaction/list",
   TransactionController.listTransactions.bind(TransactionController)
 );
 router.get(
   "/item/list",
   TransactionController.listItems.bind(TransactionController)
+);
+
+router.get(
+  "/transaction/findById",
+  TransactionController.getTransactionById.bind(TransactionController)
+);
+
+router.get(
+  "/item/findById",
+  TransactionController.getItemById.bind(TransactionController)
 );
 
 export default router;
