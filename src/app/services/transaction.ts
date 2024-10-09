abda108591b34e8305acd963ce6d5a62a4cbcab6
Gustavo Veiga São Leandro <55115090+guta307@users.noreply.github.com>
2024-10-09 16:52:31 -0300

    feat: Implement TransactionService for creating, listing, finding, and deleting transactions
    - Implement `createTransaction` to create new transactions with support for database transactions.
    - Implement `listTransactions` to retrieve transactions based on provided conditions.
    - Implement `findTransactionById` to fetch a specific transaction by ID with additional conditions.
    - Implement `deleteTransactionById` to delete a transaction by its ID, ensuring transaction integrity.
    - Handle success and error responses consistently across all methods.

--- src/app/services/transaction.ts
+++ src/app/services/transaction.ts
@@ -1,20 +1,77 @@
-import tryCatch from "../decorators/tryCatch";
 import Transaction from "../models/transaction.model";
 
 export class TransactionService {
   private static sequelizeModelTransaction = Transaction;
 
-  @tryCatch()
   public static async createTransaction(
     transactionData: any,
     transaction: any
-  ): Promise<void> {
-    await this.sequelizeModelTransaction.create(transactionData, {
-      transaction,
+  ): Promise<any> {
+    try {
+      const newTransaction = await this.sequelizeModelTransaction.create(
+        transactionData,
+        {
+          transaction,
+        }
+      );
+      return { success: true, data: newTransaction }; // Retorna um objeto com status de sucesso
+    } catch (error) {
+      console.log(error);
+      return { success: false, error }; // Retorna o erro com detalhes
+    }
+  }
+
+  public static async listTransactions(conditions: any) {
+    return await this.sequelizeModelTransaction.findAll({
+      where: conditions.where,
     });
   }
 
-  public static async listTransactions() {
-    return await this.sequelizeModelTransaction.findAll();
+  // Método para encontrar uma transação pelo ID
+  public static async findTransactionById(
+    transactionId: number,
+    whereCondition: Record<string, any>
+  ): Promise<any> {
+    try {
+      const transaction = await this.sequelizeModelTransaction.findOne({
+        where: {
+          id: transactionId, // Aqui o ID
+          ...whereCondition, // Outras condições que você queira adicionar
+        },
+      });
+      if (!transaction) {
+        return { error: `Transaction with ID ${transactionId} not found` };
+      }
+      return { success: true, data: transaction }; // Retorna um objeto com status de sucesso
+    } catch (error) {
+      return { success: false, error }; // Retorna o erro com detalhes
+    }
+  }
+
+  // Método para deletar uma transação pelo ID
+  public static async deleteTransactionById(
+    transactionId: number,
+    transaction: any
+  ): Promise<any> {
+    try {
+      const existingTransaction = await this.sequelizeModelTransaction.findByPk(
+        transactionId
+      );
+      if (!existingTransaction) {
+        return { error: `Transaction with ID ${transactionId} not found` };
+      }
+
+      await this.sequelizeModelTransaction.destroy({
+        where: { id: transactionId },
+        transaction,
+      });
+
+      return {
+        success: true,
+        message: `Transaction with ID ${transactionId} deleted successfully`,
+      };
+    } catch (error) {
+      return { success: false, error: error }; // Retorna o erro em caso de falha
+    }
   }
 }
