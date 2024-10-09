731f4fb0a502cead50b6f297ca5896d157be5031
Gustavo Veiga São Leandro <55115090+guta307@users.noreply.github.com>
2024-10-09 16:55:45 -0300

    feat: Implement Transaction and Item operations in TransactionController
    - Added methods to `TransactionController` for managing transactions and items.
    - Methods include creating, adding, subtracting, and updating items and transactions.
    - Validation is handled via `Joi` schemas, ensuring correct input structures.
    - Authorization via `validateAndAuthorize` for secure access to endpoints.
    - Added error handling with `AppError` for better error messages and codes.
    - Used Sequelize transactions for database integrity and rollback in case of failure.

--- src/app/controllers/transactionController/index.ts
+++ src/app/controllers/transactionController/index.ts
@@ -2,7 +2,11 @@ import tryCatch from "../../decorators/tryCatch";
 import { Request, Response } from "express";
 import Joi from "joi";
 import AppError from "../../appError";
-import { INVALID_FIELDS } from "../../constants/errorCodes";
+import {
+  INVALID_FIELDS,
+  LIMIT_EXCEEDED,
+  NOT_FOUND,
+} from "../../constants/errorCodes";
 
 import { sequelize } from "../../models";
 import {
@@ -11,27 +15,21 @@ import {
 } from "../../utils/validateAndAuthorize";
 import { TransactionService } from "../../services/transaction";
 import { ItemService } from "../../services/item";
+import { Op } from "sequelize";
 
 export class TransactionController {
   @tryCatch()
-  static async addItem(req: Request, res: Response): Promise<Response<any>> {
+  static async createItem(req: Request, res: Response): Promise<Response<any>> {
     const schema = Joi.object({
-      Transaction: {
-        type: Joi.boolean().required(),
-        amount: Joi.number().precision(2).required(),
-        date: Joi.date().iso().required(),
-      },
-      Item: {
-        name: Joi.string().required(),
-        description: Joi.string().optional(),
-        amount: Joi.number().integer().required(),
-        price: Joi.number().precision(2).required(),
-      },
+      name: Joi.string().required(),
+      description: Joi.string().optional(),
+
+      price: Joi.number().precision(2).required(),
     });
 
     const { error } = schema.validate(req.body);
     if (error) {
-      throw new AppError(INVALID_FIELDS, "invalid transaction structure", 400);
+      throw new AppError(INVALID_FIELDS, "invalid item structure", 400);
     }
 
     const transaction = await sequelize.transaction();
@@ -41,31 +39,164 @@ export class TransactionController {
     // Verifica se o tipo de `access` é `ValidResponse`
     if (isValidResponse(access)) {
       const existingItem = await ItemService.listItems({
-        where: { name: req.body.Item.name },
+        where: { name: req.body.name, companie_id: access.companyId },
       });
-      console.log(existingItem);
+
       if (existingItem.length > 0) {
         await transaction.rollback();
-        throw new AppError(INVALID_FIELDS, "Record already exist", 400);
+        throw new AppError(INVALID_FIELDS, "Record already exists", 400);
       }
-      console.log(access);
-      const newItem = await ItemService.createItem(
-        { ...req.body.Item, companie_id: access.companyId },
-        transaction
-      );
 
-      await TransactionService.createTransaction(
-        { ...req.body.Transaction, item_id: newItem.item_id },
+      await ItemService.createItem(
+        { ...req.body, companie_id: access.companyId, amount: 0 },
         transaction
       );
 
       await transaction.commit();
 
       return res.status(201).json({
-        message: "Item and Transaction created successfully.",
+        message: "Item created successfully.",
       });
     } else {
       // Se for uma resposta do tipo HTTP, retorna diretamente
+      return access;
+    }
+  }
+
+  @tryCatch()
+  static async addItem(req: Request, res: Response): Promise<Response<any>> {
+    const schema = Joi.object({
+      item_id: Joi.number().integer().required(),
+      amount: Joi.number().integer().positive().required(),
+    });
+
+    const { error } = schema.validate(req.body);
+    if (error) {
+      throw new AppError(INVALID_FIELDS, "invalid item structure", 400);
+    }
+
+    const transaction = await sequelize.transaction();
+
+    const access = await validateAndAuthorize(req, res);
+
+    if (isValidResponse(access)) {
+      const existingItem = await ItemService.findItem({
+        where: { item_id: req.body.item_id, companie_id: access.companyId },
+      });
+
+      if (!existingItem) {
+        await transaction.rollback();
+        throw new AppError(
+          INVALID_FIELDS,
+          `Item com o ID '${req.body.item_id}' não existe.`,
+          404
+        );
+      }
+      const newamount = existingItem.dataValues.amount + req.body.amount;
+      await ItemService.updateItem(
+        existingItem,
+        { amount: newamount },
+        transaction
+      );
+
+      const transactionValue = req.body.amount * existingItem.dataValues.price;
+
+      await TransactionService.createTransaction(
+        {
+          companie_id: access.companyId,
+          type: false, // false para entrada
+          amount: transactionValue,
+          item_id: existingItem.dataValues.item_id,
+          date: new Date(),
+        },
+        transaction
+      );
+
+      await transaction.commit();
+      return res.status(200).json({
+        message: "Item adicionado com sucesso e transação registrada.",
+      });
+    } else {
+      return access;
+    }
+  }
+
+  @tryCatch()
+  static async subtractItem(
+    req: Request,
+    res: Response
+  ): Promise<Response<any>> {
+    const schema = Joi.object({
+      item_id: Joi.number().integer().required(),
+      amount: Joi.number().integer().positive().required(),
+    });
+
+    const { error } = schema.validate(req.body);
+    if (error) {
+      throw new AppError(INVALID_FIELDS, "invalid item structure", 400);
+    }
+
+    const transaction = await sequelize.transaction();
+
+    const access = await validateAndAuthorize(req, res);
+
+    if (isValidResponse(access)) {
+      const existingItem = await ItemService.findItem({
+        where: { item_id: req.body.item_id, companie_id: access.companyId },
+      });
+
+      if (!existingItem) {
+        await transaction.rollback();
+        throw new AppError(
+          INVALID_FIELDS,
+          `Item com o ID '${req.body.item_id}' não existe.`,
+          404
+        );
+      }
+
+      if (existingItem.dataValues.amount === 0) {
+        await transaction.rollback();
+        throw new AppError(
+          LIMIT_EXCEEDED,
+          `Não há itens em estoque para subtrair.`,
+          400
+        );
+      }
+
+      if (existingItem.dataValues.amount < req.body.amount) {
+        await transaction.rollback();
+        throw new AppError(
+          LIMIT_EXCEEDED,
+          `Quantidade insuficiente no estoque.`,
+          400
+        );
+      }
+
+      const newamount = existingItem.dataValues.amount - req.body.amount;
+      await ItemService.updateItem(
+        existingItem,
+        { amount: newamount },
+        transaction
+      );
+
+      const transactionValue = req.body.amount * existingItem.dataValues.price;
+
+      await TransactionService.createTransaction(
+        {
+          companie_id: access.companyId,
+          type: true, // true para saída
+          amount: transactionValue,
+          item_id: existingItem.dataValues.item_id,
+          date: new Date(),
+        },
+        transaction
+      );
+
+      await transaction.commit();
+      return res.status(200).json({
+        message: "Item subtraído com sucesso e transação registrada.",
+      });
+    } else {
       return access;
     }
   }
@@ -142,8 +273,100 @@ export class TransactionController {
 
     // Verifica se o tipo de `access` é `ValidResponse`
     if (isValidResponse(access)) {
-      const transactions = await TransactionService.listTransactions();
+      // Captura os parâmetros opcionais da requisição
+      const { startDate, endDate, itemId, type } = req.query;
+
+      // Cria a condição de filtro base para a consulta
+      const whereCondition: any = {};
+
+      // Filtra por intervalo de datas, se fornecido
+      if (startDate && endDate) {
+        whereCondition.date = {
+          [Op.between]: [
+            new Date(startDate as string).toISOString(),
+            new Date(endDate as string).toISOString(),
+          ],
+        };
+      } else if (startDate) {
+        whereCondition.date = {
+          [Op.gte]: new Date(startDate as string).toISOString(),
+        };
+      } else if (endDate) {
+        whereCondition.date = {
+          [Op.lte]: new Date(endDate as string).toISOString(),
+        };
+      }
+
+      // Filtra pelo itemId, se fornecido
+      if (itemId) {
+        whereCondition.item_id = parseInt(itemId as string, 10);
+      }
+
+      // Filtra pelo tipo, se fornecido (supondo que `type` seja um boolean ou string)
+      if (type) {
+        whereCondition.type = type === "true"; // Converte para booleano
+      }
+      whereCondition.companie_id = access.companyId;
+      // Executa a consulta filtrada
+      const transactions = await TransactionService.listTransactions({
+        where: whereCondition,
+      });
+
       return res.status(200).json(transactions);
+    } else {
+      // Se for uma resposta do tipo HTTP, retorna diretamente
+      return access;
+    }
+  }
+
+  @tryCatch()
+  static async getTransactionById(
+    req: Request,
+    res: Response
+  ): Promise<Response<any>> {
+    // Valida e autoriza o usuário antes de listar a transação
+    const access = await validateAndAuthorize(req, res);
+
+    if (isValidResponse(access)) {
+      // Validação do parâmetro ID da query string
+      const schema = Joi.object({
+        transactionId: Joi.string().required(), // Aceita string que será convertida
+      });
+
+      const { error } = schema.validate(req.query);
+      if (error) {
+        throw new AppError(
+          INVALID_FIELDS,
+          "Invalid transaction ID format",
+          400
+        );
+      }
+
+      const transactionId = parseInt(req.query.transactionId as string, 10); // Transforma de string para número
+
+      if (isNaN(transactionId)) {
+        throw new AppError(
+          INVALID_FIELDS,
+          "Transaction ID must be a number",
+          400
+        );
+      }
+
+      // Busca a transação pelo ID
+      const transaction = await TransactionService.findTransactionById(
+        transactionId,
+        {}
+      );
+
+      if (!transaction) {
+        throw new AppError(
+          NOT_FOUND,
+          `Transaction with ID ${transactionId} not found`,
+          404
+        );
+      }
+
+      return res.status(200).json(transaction);
     } else {
       // Se for uma resposta do tipo HTTP, retorna diretamente
       return access;
@@ -157,8 +380,51 @@ export class TransactionController {
 
     // Verifica se o tipo de `access` é `ValidResponse`
     if (isValidResponse(access)) {
-      const items = await ItemService.listItems();
+      const items = await ItemService.listItems({
+        where: { companie_id: access.companyId },
+      });
       return res.status(200).json(items);
+    } else {
+      // Se for uma resposta do tipo HTTP, retorna diretamente
+      return access;
+    }
+  }
+
+  @tryCatch()
+  static async getItemById(
+    req: Request,
+    res: Response
+  ): Promise<Response<any>> {
+    // Valida e autoriza o usuário antes de listar o item
+    const access = await validateAndAuthorize(req, res);
+
+    if (isValidResponse(access)) {
+      // Validação do parâmetro ID da query string
+      const schema = Joi.object({
+        itemId: Joi.string().required(), // Aceita string que será convertida
+      });
+
+      const { error } = schema.validate(req.query);
+      if (error) {
+        throw new AppError(INVALID_FIELDS, "Invalid item ID format", 400);
+      }
+
+      const itemId = parseInt(req.query.itemId as string, 10); // Transforma de string para número
+
+      if (isNaN(itemId)) {
+        throw new AppError(INVALID_FIELDS, "Item ID must be a number", 400);
+      }
+
+      // Busca o item pelo ID
+      const item = await ItemService.findItem({
+        where: { item_id: itemId, companie_id: access.companyId },
+      });
+
+      if (!item) {
+        throw new AppError(NOT_FOUND, `Item with ID ${itemId} not found`, 404);
+      }
+
+      return res.status(200).json(item);
     } else {
       // Se for uma resposta do tipo HTTP, retorna diretamente
       return access;
