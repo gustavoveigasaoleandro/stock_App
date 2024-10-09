733602946e18b165b54fbf961378d7dc112884f5
Gustavo Veiga São Leandro <55115090+guta307@users.noreply.github.com>
2024-10-09 16:56:21 -0300

    feat: Set up Express server with routing, model synchronization, and RabbitMQ consumer
    - Configured an Express server to run on port 3000.
    - Integrated application routes with `routes(app)` for handling requests.
    - Initialized RabbitMQ consumer using `startConsumer` to listen for messages.
    - Added error handling middleware `errorHandler` for centralized error management.
    - Synchronized Sequelize models to ensure the database schema is up-to-date with `syncModels()`.

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,15 @@ import express from "express";
 import { syncModels } from "./app/models/index.js";
 import routes from "./app/routes/index.js";
 import errorHandler from "./app/middleware/errorHandler.js";
-import { startApp } from "./app/utils/rabbitMQ/setup.js";
+import { startConsumer } from "./app/utils/validateAndAuthorize/index.js";
 
 const app = express();
 
 const port = 3000;
 
-startApp();
-
 routes(app);
 
+startConsumer().catch(console.error);
 app.listen(port, () => {
   console.log(`O server esta rodando na porta ${port}`);
 });
